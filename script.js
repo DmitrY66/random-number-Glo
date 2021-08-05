f17fedc4d042544be3c35e3cd2a6bb893bdb9c0e
Dmitri Gorun <iii448@mail.ru>
2021-08-06 02:25:54 +0500

    lesson06-function-scope-closure

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 'use strict';
 
+let playGuessNumber = function () {
 
+  let randomNumber;
+
+  let getRandom = function () {
+    randomNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    console.log('randomNumber: ', randomNumber);
+  };
+
+  getRandom();
+
+  let guess = function () {
+
+    let userNumber = +prompt('Угадай число от 1 до 100');
+
+    let check = function () {
+      if (isNaN(userNumber)) {
+        userNumber = +prompt('Виедите число');
+        check();
+      } else if (userNumber === +null) {
+        alert('Игра закончена.');
+        playGuessNumber();
+      }
+    };
+    check();
+
+    let compare = function () {
+      if (userNumber === randomNumber) {
+        alert('Поздравляю, Вы угадали!!!');
+      }
+      if (userNumber > randomNumber) {
+        userNumber = +prompt('Загаданное число меньше. Введите новый вариант ответа.');
+        check();
+        compare();
+      }
+      if (userNumber < randomNumber) {
+        userNumber = +prompt('Загаданное число больше. Введите новый вариант ответа.');
+        check();
+        compare();
+        
+      }
+    };
+    compare();
+  };
+  guess();
+};
+
+playGuessNumber();
 
 
 
